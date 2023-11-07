8a6ca649afc1f28a765efbd701652d185eb08847
opera7133 <ainznino@pm.me>
2023-11-07 11:46:58 +0900

    d

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
 // Create a new client instance
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
 });
 const mail = process.env.MAIL_ACCOUNT;
 const pass = process.env.MAIL_PASSWORD;
-
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: mail,
-    pass: pass,
-  },
-});
-
+sgMail.setApiKey(pass);
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, (c) => {
@@ -24,12 +20,11 @@ client.once(Events.ClientReady, (c) => {
 });
 
 client.on(Events.MessageCreate, async (msg) => {
-  console.log(msg);
-  await transporter.sendMail({
-    from: "m21092@g.metro-cit.ac.jp", // 送信元メールアドレス
+  await sgMail.send({
+    from: { email: "metro-cit@ja1ykl.com", name: "D2Email" }, // 送信元メールアドレス
     to: process.env.MAIL_TO_ACCOUNT, // 送信先メールアドレス
     subject: "Discord通知：" + msg.author.displayName,
-    text: msg.author.displayName + ": " + msg.content,
+    text: msg.author.displayName + ": " + msg.content + "\n" + "in #" + msg.channel.name,
   });
 });
 
